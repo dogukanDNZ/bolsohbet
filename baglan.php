5ed8a3c52517e79ab5ae83383e3b9247718fe6bd
dogukanDNZ <56682793+dogukanDNZ@users.noreply.github.com>
2020-06-07 00:27:05 +0200

    Add files via upload

---
+++ baglan.php
@@ -0,0 +1,10 @@
+﻿<?php
+	session_start();
+	ob_start();
+	@$bag=mysql_connect("Adres","Veritabanı Kullanıcı Adı","Veritabanı Şifresi")or die("Bağlantı Başarısız!");
+	@$sec=mysql_select_db("Veritabanı İsimi",$bag)or die("Veritabanı Yok!");
+	mysql_query("SET NAMES utf8");
+	mysql_query("SET CHARACTER SET utf8");
+	error_reporting(0);
+	date_default_timezone_set('Europe/Istanbul');
+?>
